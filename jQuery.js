de806675b8ddf3d2ed69a8c1e2eb2683b75df07d
Joseph Lozano <lozanojoseph@gmail.com>
2015-08-25 09:35:17 -0700

    make constructor

--- jQuery.js
+++ jQuery.js
@@ -1,21 +1,45 @@
+function JQ(collection){
+  this.length = collection.length;
+
+  this.idx = function(num){
+    return collection[num]
+  };
+
+
+}
+
 function jQuery(string) {
+  //
+  //if typeof(string) == ojbect(dom)
+  //return new JQ(string)
+  //
+  //
   var search_type = string[0];
   switch (search_type) {
     case ".":
-      return document.getElementsByClassName(string.slice(1,string.length));
+      var arr = document.getElementsByClassName(string.slice(1,string.length));
+      var jq = new JQ(arr);
+      for (var i = 0; i < jq.length; i++){
+        jq[i] = arr[i];
+      }
+      return jq;
     case "#":
       var elem = document.getElementById(string.slice(1,string.length));
       if (elem === null) {
-        return [];
+        return new JQ();
       } else {
-        return elem;
+        var jq =  new JQ(elem);
+        jq.length = 1;
+        return jq;
       }
     default:
-      return document.getElementsByTagName(string);
+      var arr = document.getElementsByTagName(string);
+      var jq = new JQ(arr);
+            for (var i = 0; i < jq.length; i++){
+        jq[i] = arr[i];
+      }
+      return jq
   }
 }
 
-var joQuery = {
-  
-}
 
