707adf8e16cb053f5bf41f2c5ca91bf2e86657c5
alokp <alokpra@gmail.com>
2015-08-25 11:00:02 -0700

    working on jquery mock object methods

--- page.js
+++ page.js
@@ -1,41 +1,31 @@
 // window.onload = function() {
-
-  //Without this line, does not work, why?
+  // Without this line, does not work, why? Ans: JS has function level scope
   // window.jQuery = jQuery;
-  
-  function jQuery(str) {
 
-    switch (str[0]) {
-      case '.':
-        return document.getElementsByClassName(str.slice(1, str.length));
-      case '#':
-        return document.getElementById(str.slice(1, str.length));
-      default:
-        return document.getElementsByTagName(str);
-    }
- 
+// Function to find jQuery object
+var jQuery = function (str) {
+
+  var jQ = new jQuery_object();
+
+  switch (str[0]) {
+    case '.':
+      jQ.collection.push(document.getElementsByClassName(str.slice(1, str.length)));
+      return jQ;
+    case '#':
+      jQ.collection.push(document.getElementById(str.slice(1, str.length)));
+      return jQ;
+    default:
+      jQ.collection.push(document.getElementsByTagName(str));
+      return jQ;
   }
-// };
+
+};
+
+// Constructor for jQuery object
+function jQuery_object () {
+  this.collection = [];
 
 
-  // var id = function(str) {
-  //   return document.getElementById(str);
-  // };
-
-  // var tag = function(str) {
-  //   return document.getElementsByTagName(str);
-  // };
-
-  // var className = function(str) {
-  //   return document.getElementsByClassName(str);
-  // };
-
-  // this.selector = function(str) {
 
 
- // return this.result;
-  // };
-
-  // this.result = "";
-
-
+};
