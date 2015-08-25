11ab23356fd0724460c304002262c993ff05f393
Alice Fung <alice.o.fung@gmail.com>
2015-08-25 13:22:00 -0400

    working on jquery obj

--- build_jquery.js
+++ build_jquery.js
@@ -1,51 +1,67 @@
-var jQuery = function (input){
-  //currently returns null instead of [] if not found
-  var result;
+// create jQuery obj
+// constructor function
+
+function jQuery(input){
+  var result = {}; // initialize with empty jQuery object
+  var arr;
+
   if (typeof input === "object") {
-    return new jQueryObj([input]);
+    result.obj = [input];
   }
   else if (input[0] === "."){
-    result = window.document.getElementsByClassName(input.substring(1));
-    return new jQueryObj(result);
+    arr = window.document.getElementsByClassName(input.substring(1));
+    result.obj = arr;
   }
   else if (input[0]=== '#'){
-    result = window.document.getElementById(input.substring(1));
-    return new jQueryObj(result);
+    arr = window.document.getElementById(input.substring(1));
+    // if no result, replace null with []
+    if (arr === null){
+      result.obj = [];
+    }
+    else {
+      result.obj = [arr];
+    }
   }
   else {
-    result = window.document.getElementsByTagName(input);
-    return new jQueryObj(result);
+    arr = window.document.getElementsByTagName(input);
+    result.obj = arr;
   }
 
-};
+
+  // if (result) {
+  //   // this.result = [];
+  //   arr = [];
+  //   result.forEach(function(ele) {
+  //     arr.push(jQuery(ele));
+  //   });
+  //   this.result = arr;
+  // }
+  // else {
+  //   this.result = [];
+  // }
+
+  result.length = function() {
+    return result.obj.length; // returns a number
+  },
+
+  // result.foo = function(pos) {
+  //   return "foo method";
+  // },
+
+  result.idx = function(pos) {
+    // return this.result[pos]; //currently returns DOM obj
+    return jQuery(result.obj[pos]);
+  };
+
+
+  return result;
+}
 
 // Alias function
 var $ = function(input) {
   return jQuery(input);
-}
+};
 
-// create jQuery obj
-// constructor function
-
-function jQueryObj(result){
-  if (result) { 
-    this.result = [];
-    result.forEach(function(ele) {
-      this.result.push(jQuery(ele));
-    })
-  }
-  else {
-    this.result = [];
-  }
-
-  this.length = function() {
-    return this.result.length;
-  },
-
-  this.idx = function(pos) {
-    return this.result[pos];
-  }
-}
 
 
 
