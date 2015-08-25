40c3e4b00a0942390957bcd4afe824f7d56311de
shadefinale <shadefinale@gmail.com>
2015-08-25 12:18:55 -0500

    Begin work on height.

--- scripts.js
+++ scripts.js
@@ -38,19 +38,68 @@ function jQuery(arg){
 
 		this.addClass = function(name){
 			for (var i = 0; i < this.elements.length; i++) {
-				this.elements[i].className += " " + name
+				this.elements[i].className += " " + name;
+				this.elements[i].className = this.elements[i].className.replace(/^[ \t]+/, "");
 			};
-			
-		}
+
+		};
 
 		this.removeClass = function(name){
 			for (var i = 0; i < this.elements.length; i++) {
-				this.elements[i].className.replace(new RegExp(name, "g"), "");
-				this.elements[i].className.replace(/[ ]{2,}/g, " ");
-				this.elements[i].className.replace(/[ \t]+/, "");
+				var cElement = this.elements[i];
+				cElement.className = cElement.className.replace(new RegExp(name, "g"), "");
+				// Condence pairs of 2+ spaces into one space
+				cElement.className = cElement.className.replace(/[ ]{2,}/g, " ");
+				// Remove trailing spaces
+				cElement.className = cElement.className.replace(/[ \t]+$/, "");
+				// Remove leading spaces
+				cElement.className = cElement.className.replace(/^[ \t]+/, "");
 			};
-			
-		}
+
+		};
+
+		this.toggleClass = function(name){
+			// Remove the element if the class has it, add it if it doesn't.
+			for (var i = 0; i < this.elements.length; i++) {
+				if (this.elements[i].className.match(new RegExp(name, "i"))) {
+					var cElement = this.elements[i];
+					cElement.className = cElement.className.replace(new RegExp(name, "g"), "");
+					// Condence pairs of 2+ spaces into one space
+					cElement.className = cElement.className.replace(/[ ]{2,}/g, " ");
+					// Remove trailing spaces
+					cElement.className = cElement.className.replace(/[ \t]+$/, "");
+					// Remove leading spaces
+					cElement.className = cElement.className.replace(/^[ \t]+/, "");
+				} else {
+					this.elements[i].className += " " + name;
+					this.elements[i].className = this.elements[i].className.replace(/^[ \t]+/, "");
+				}
+			};
+		};
+
+		this.val = function(arg){
+			if (arg == undefined){
+				return this.elements[0].innerHTML;
+			} else {
+				for (var i = 0; i < this.elements.length; i++){
+					this.elements[i].innerHTML = arg;
+				}
+			}
+		};
+
+		this.css = function(property, newVal){
+			if (arguments.length == 1){
+				return this.elements[0].style[property];
+			} else if (arguments.length > 1){
+				for (var i = 0; i < this.elements.length; i++){
+					this.elements[i].style[property] = newVal;
+				}
+			}
+		};
+
+		this.height = function(newHeight){
+
+		};
 
 	}
 	else{
