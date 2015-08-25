3af3e71b4accef8c24df80cb54cfea61fb458dfb
mikebutts <mgbutts@gmail.com>
2015-08-25 12:53:04 -0400

    removeClass

--- scripts.js
+++ scripts.js
@@ -26,11 +26,32 @@ function jQuery(arg){
 			};
 		})(arg);
 		this.length = this.elements.length;
+
 		this.idx = function(pos){ return this.elements[pos] };
+
 		this.hasClass = function(name){
-			console.log(name);
-			return !!(this.elements[0].className.match(new RegExp(name, "i")))
+			for (var i = 0; i < this.elements.length; i++) {
+				if (this.elements[i].className.match(new RegExp(name, "i"))) return true;
+			};
+			return false;
 		}
+
+		this.addClass = function(name){
+			for (var i = 0; i < this.elements.length; i++) {
+				this.elements[i].className += " " + name
+			};
+			
+		}
+
+		this.removeClass = function(name){
+			for (var i = 0; i < this.elements.length; i++) {
+				this.elements[i].className.replace(new RegExp(name, "g"), "");
+				this.elements[i].className.replace(/[ ]{2,}/g, " ");
+				this.elements[i].className.replace(/[ \t]+/, "");
+			};
+			
+		}
+
 	}
 	else{
 		return new jQuery(arg);
