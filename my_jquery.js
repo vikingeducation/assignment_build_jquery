fc14dc03d95125f91e1bbf8698863a83246b9378
Adam <adam.kinson@gmail.com>
2015-09-04 12:27:06 -0400

    finish has,add,remove,toggleClass

--- my_jquery.js
+++ my_jquery.js
@@ -23,6 +23,47 @@ var jQuery = function(input) {
     this.idx = function(index) {
       return this.collection[index];
     }
+
+    this.each = function(myFunc) {
+      for(var i = 0; i < this.collection.length; i++) {
+        myFunc(this.collection[i], i);
+      };
+    }
+
+    this.hasClass = function(className) {
+      var node = this.collection[0];
+      return (node.getElementsByClassName(className).length > 0) ? true : false;
+    }
+
+    this.addClass = function(newClass) {
+      this.each(function(el) {
+        el.className = (el.className + " " + className).trim();
+      });
+    }
+
+    this.removeClass = function(classToRemove) {
+      this.each(function(el) {
+        currentClasses = el.className
+        el.className = currentClasses.replace(classToRemove, "").trim();
+      });
+    }
+
+    this.toggleClass = function(className, state) {
+      var regex = new RegExp("(^| )" + className + "( |$)")
+
+      // when state not given
+      this.each(function(el) {
+        if (regex.test(el.className)) {
+          currentClasses = el.className
+          el.className = currentClasses.replace(className, "").trim();
+        }
+        else {
+          el.className = (el.className + " " + className).trim();
+        }
+      })
+    }
+
+
   }
 
   return new jQuery();
