05ba9c10109621c3589212645221f0110fea33cf
telwell <me@trevorelwell.com>
2015-10-06 21:57:43 -0400

    methods up to val

--- jQuery.js
+++ jQuery.js
@@ -18,13 +18,13 @@ function jQuery(input){
   // Search the document for the proper collection
   switch(input_type) {
     case "class":
-      collection = document.getElementsByClassName(input);
+      collection = document.getElementsByClassName(input.slice(1));
       break;
     case "id":
-      collection = document.getElementById(input.slice(1));
+      collection = Array(document.getElementById(input.slice(1)));
       break;
     case "node":
-      collection = node;
+      collection = [node];
       break;
     case "element":
       collection = document.getElementsByTagName(input);
@@ -38,7 +38,7 @@ function jQuery(input){
       return collection[index].innerHTML;
     },
     
-    // Method which cycles through every item in the collection
+    // Method which cycles through every item in the collection. Or just run the function on the first element if no length is defined.
     each: function(f){
       if(!collection.length){
         f(collection);
@@ -49,7 +49,7 @@ function jQuery(input){
       }
     },
     
-    // Takes a class name and returns whether or not the particular collection has that class. Only works for collections of lenght 1.
+    // Takes a class name and returns whether or not the particular collection has that class.
     hasClass: function(searchClass){
       var trigger = false;
       this.each(function(element){
@@ -63,7 +63,53 @@ function jQuery(input){
     addClass: function(addClassName){
       this.each(function(element){
         element.className += " " + addClassName;
+        console.log(element.className);
       });
+      return this;
+    }, 
+    
+    // QUESTION: This is giving me problems when I use the class that's first in the 
+    // className. What gives?
+    removeClass: function(removeClassName){
+      var reg = new RegExp(removeClassName, "g");
+      this.each(function(element){
+        element.className = element.className.replace(reg, '').trim();
+        console.log(element.className);
+      });
+      return this;
+    }, 
+    
+    toggleClass: function(toggleClassName){
+      var reg = new RegExp(toggleClassName, "g");
+      this.each(function(element){
+        index_toggle_class = element.className.split(' ').indexOf(toggleClassName);
+        if( index_toggle_class >= 0){
+          // DUP 1: This is the same ass remove class... how can I use that method?
+          element.className = element.className.replace(reg, '').trim();
+          console.log(element.className);
+        } else {
+          // DUP 2: Same code as addClass... how can I use that method?
+          element.className += " " + toggleClassName;
+          console.log(element.className);
+        }
+        return this;
+      });
+    }, 
+    
+    val: function(value){
+      // If nothing is passed in for value then
+      // we'll run the top part of the if statement
+      if(typeof value == 'undefined'){
+        if(!!collection[0].value){
+          return collection[0].value;
+        } else {
+          return null;
+        }
+      // Add the ability to set value as well.
+      } else {
+        collection[0].value = value;
+        return collection[0].value;
+      }
     }
   };
   
@@ -75,5 +121,8 @@ function $(input){
   return jQuery(input);
 }
 
-var node = document.getElementById("content");
-$('.my-class').addClass("second-class");
+
+// Testing
+var node = document.getElementById("some-input");
+var test = $(node).val("nest");
+console.log(test);
