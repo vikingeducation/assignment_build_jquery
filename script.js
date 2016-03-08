340c335355ac48138f490f277ac0bc16dde29002
Deepa Kackar <dkackar99@yahoo.com>
2016-03-08 09:48:17 -0800

    Added some jquery function

--- script.js
+++ script.js
@@ -1,9 +1,46 @@
 
-function FooConstructor(){
-  this.Prop = "foo!";
-  this.Method = funtion() { 
-    return "Hello"}
-} 
+function FooConstructor() {
+  this.fooProp = "foo!";
+  this.fooMethod = function() {
+    return "Hello"
+  };
+}
 
 var foo = new FooConstructor();
-foo
+foo;
+
+function BarConstructor() {
+  this.someProp = "xyz";
+  this.someMethod = function() {
+    return "Hello"
+  };
+  return this;
+}
+
+var bar = BarConstructor();
+
+
+
+function SimpleObject() {
+  this.collection = [1,2,3,"string"];
+  this.each = function(func) {
+    for (var i = 0; i < this.collection.length; i++) {
+      func(this.collection[i],i);
+    }
+  };
+}
+
+
+function jQuery(str) {
+
+  switch(str[0]) {
+    case "." :
+      return document.getElementsByClassName(str.slice(1));
+      break;
+    case "#" :
+      return document.getElementById(str.slice(1));
+      break;
+    default :
+      return document.getElementsByTagName(str);
+  }
+}
