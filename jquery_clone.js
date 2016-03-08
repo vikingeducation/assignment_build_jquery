d7413770c829ea4c0e33024dd85fb1c75d7155b9
Joshua Masland-Sarani <j.masland@gmail.com>
2016-03-08 12:30:40 -0500

    start jQuery

--- jquery_clone.js
+++ jquery_clone.js
@@ -1,4 +1,40 @@
 
+function jQuery( selector ) {
+  switch (selector.charAt(0)) {
+  case "#":
+    var element = document.getElementById(selector.slice(1));
+    break;
+  case ".":
+    var element = document.getElementsByClassName(selector.slice(1));
+    break;
+  default:
+    var element = document.getElementsByTagName(selector);
+  }
+  return new JQueryReturn(element);
+}
+
+var $ = jQuery;
+
+function JQueryReturn() {
+
+}
+
+JQueryReturn.prototype.hasClass = function(className) {
+
+}
+
+// hasClass() (Docs)
+// addClass() (Docs)
+// removeClass() (Docs)
+// toggleClass() (Docs)
+// val() (Docs) Getter & Setter
+// css() (Docs) Getter & Setter
+// height() (Docs) Getter & Setter
+// width() (Docs) Getter & Setter
+// attr() (Docs) Getter & Setter
+// html() (Docs) Getter & Setter
+
+
 function FooConstructor() {
   this.ourProp = { josh: "tetris", koz: "hotdog"}
 }
@@ -32,5 +68,3 @@ myObj.collection = [1,"foo",3];
 myObj.each( function( el, index ){
     console.log( "Item " + index + " is " + el)
 });
-
-
