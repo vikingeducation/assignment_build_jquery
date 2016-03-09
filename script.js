d483681c422ac151cf0436f34e9cc8d721739c88
Deepa Kackar <dkackar99@yahoo.com>
2016-03-08 16:13:31 -0800

    Added the val and css

--- script.js
+++ script.js
@@ -90,10 +90,27 @@ function jQuery(str) {
       else{
         this.addClass(str);
       }  
+    },
+
+    val: function(input){
+      if(input){
+        //This does not reflect in the document object but
+        //changes are seen on the form
+        this.collection[0].value = input;
+      }
+      else {
+        return this.collection[0].value;
+      }
+    },
+
+    css: function(css_selector,input){
+      if(input){
+        collection[0].style[css_selector] = input;
+      }
+      else {
+        return collection[0].style[css_selector];
+      }
     }
-
-
-
 
   }
 
