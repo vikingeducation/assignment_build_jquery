fa3abcb63d49f5f208fe376752eb25fbe5a824d8
Sia <siakaramalegos@gmail.com>
2016-03-31 15:56:09 -0500

    Basic jQuery selects, index, length

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
 var jQuery = function(selector){
-  if (selector.charAt(0) === '#') {
-    return window.document.getElementById(selector.slice(1));
-  } else if (selector.charAt(0) === '.') {
-    return window.document.getElementsByClassName(selector.slice(1));
-  } else {
-    return window.document.getElementsByTagName(selector);
+  if (!(this instanceof jQuery)) {
+    return new jQuery(selector);
   }
+
+  // Why must I put this first when before order didn't matter?
+  var getCollection = function(selector) {
+    if (selector.charAt(0) === '#') {
+      return [window.document.getElementById(selector.slice(1))];
+    } else if (selector.charAt(0) === '.') {
+      return window.document.getElementsByClassName(selector.slice(1));
+    } else {
+      return window.document.getElementsByTagName(selector);
+    }
+  };
+
+  this.selector = selector;
+  this.collection = getCollection(this.selector);
+
+  this.length = function(){
+    return this.collection.length;
+  };
+
+  this.idx = function(index){
+    return this.collection[index];
+  };
+
+  return this;
 }
