09d6d8d187f8be026e9f5d661ad67ec11d3c8836
Steven Chang <prime_pork@hotmail.com>
2016-06-27 22:38:58 +1000

    added the each method to SimpleObject function

--- build.js
+++ build.js
@@ -2,15 +2,20 @@
 function FooConstructor(){
 	this.fooMethod = function(){};
 	this.fooProp = "foo!";
-}
+};
 
 // This returns Object instead of BarConstructor, is that okay?
 BarConstructor = function(){
   return {barMethod: function(){},
   barProp: "bar!"}
-}
+};
 
 function SimpleObject(){
   this.collection = [];
   this.each = function(x){this.collection.forEach(x)};
-}
+  each = function(collection, x){
+  	console.log( "Item " + index + " is " + el);
+  };
+};
+
+SimpleObject.each = function(collection, x){ collection.forEach(x) };
