e85f661c659dd6ba886fecf8136dddb23c9abfb7
Steven Chang <prime_pork@hotmail.com>
2016-06-27 22:16:13 +1000

    warmup constructors and first anonymous task is working

--- build.js
+++ build.js
@@ -1,4 +1,16 @@
+// This returns Object instead of FooConstructor, is that okay?
 function FooConstructor(){
 	this.fooMethod = function(){};
 	this.fooProp = "foo!";
+}
+
+// This returns Object instead of BarConstructor, is that okay?
+BarConstructor = function(){
+  return {barMethod: function(){},
+  barProp: "bar!"}
+}
+
+function SimpleObject(){
+  this.collection = [];
+  this.each = function(x){this.collection.forEach(x)};
 }
