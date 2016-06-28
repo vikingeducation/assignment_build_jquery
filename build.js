2cc9b1b3d6f2d7e78dfb2490d0bdafda2f0708a7
Steven Chang <prime_pork@hotmail.com>
2016-06-28 11:55:22 +1000

    jQuery function determines what the searchedType is and removes the first character of the searched term if it is a class or an id search

--- build.js
+++ build.js
@@ -13,9 +13,33 @@ BarConstructor = function(){
 function SimpleObject(){
   this.collection = [];
   this.each = function(x){this.collection.forEach(x)};
-  each = function(collection, x){
-  	console.log( "Item " + index + " is " + el);
-  };
 };
 
 SimpleObject.each = function(collection, x){ collection.forEach(x) };
+
+// Build jQuery
+// jQuery() - first thing to note is that
+// it is a function that takes an argument
+// in our case, we're only going to take one argument
+jQuery = function( searched ) {
+	// In the HTML DOM, everything is a node
+	// The document itself is a document note
+	// All of the DOM methods that are available can get be user here: http://www.w3schools.com/jsref/dom_obj_all.asp
+
+	// 3 Searchables
+	// 1. classes
+	// 2. ids
+	// 3. elements
+	// Will get rid of that first character after we've determined what sort of type we've found
+	var searchedType = "element";
+	if ( searched[0] === "." ) {
+	  searchedType = "class";
+	  searched.substr(1);
+	} else if ( searched[0] === "#" ) {
+	  searchedType = "id";
+	  searched.substr(1);
+	};
+
+	// Testing the function up until now..
+	return searchedType;
+}
