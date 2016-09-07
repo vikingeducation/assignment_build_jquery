a4c5d7b654ca7574c7720cfce5bd31d198429855
jsteenb2 <jonathansteenbergen@gmail.com>
2016-09-07 10:50:27 -0700

    finished jQuery selectors

---
+++ jquery.js
@@ -0,0 +1,40 @@
+var jQuery = function(ele) {
+  var matches = [];
+
+  var populateCollection = function(elements){
+    if ( elements instanceof HTMLCollection ){
+      for(var i =0; i < elements.length; i++){
+        matches.push(elements.item(i));
+      }
+    }
+    else {
+      matches.push(elements)
+    }
+  };
+
+  var query = function(ele) {
+    if ( ele[0] == "#"){
+      var search = ele.slice(1);
+      var matchedElements = document.getElementById(search);
+      populateCollection(matchedElements);
+    }
+    else if ( ele[0] == "."){
+      var search = ele.slice(1);
+      var matchedElements = document.getElementsByClassName(search);
+      populateCollection(matchedElements);
+    }
+    else {
+      var matchedElements = document.getElementsByTagName(ele);
+      populateCollection(matchedElements);
+    }
+  }
+
+  this.length = matches.length;
+
+  this.idx = function(index){
+    return collection[index];
+  }
+
+  query(ele);
+  return matches;
+}
--- script.js
+++ script.js
@@ -5,10 +5,27 @@ function FooConstructor() {
   };
 }
 
-function BarConstructor (){
+function BarConstructor() {
   this.barProp = "bar!";
   this.barMethod = function() {
     console.log("Bar Function Works!");
   };
   return this;
 }
+
+function SimpleObject() {
+  this.collection = [];
+  this.each = function(fn){
+    for( var i = 0; i < this.collection.length; i++ ){
+      fn(this.collection[i], i);
+    }
+  };
+  // each:  NO WORK
+}
+
+//TODO: Ask about why we can't mix and match : and =
+SimpleObject.each = function(collection, cb){
+  for( var i = 0; i < collection.length; i++ ){
+    cb(collection[i], i);
+  }
+};
