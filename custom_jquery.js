187e235756d32716fc3a7b0fcaaa06a988dd6efc
Dylan Lynch <lynchd2@tcnj.edu>
2016-09-07 14:59:22 -0400

    jquery functions

--- custom_jquery.js
+++ custom_jquery.js
@@ -16,24 +16,27 @@
 
 function jQuery(input) {
   if (!(this instanceof jQuery)) return new jQuery(input);
-
-  var jquery_object;
+  var jquery_object = document;
 
   if (typeof input === "object") {
     jquery_object = [input];
   } else {
-    switch(input[0]) {
-      case ".":
-        jquery_object = document.getElementsByClassName(input.slice(1));
-        break;
-      case "#":
-        jquery_object = document.getElementById(input.slice(1));
-        break;
-      default:
-        jquery_object = document.getElementsByTagName(input);
-        break;
+    input = input.split(" ");
+    var counter = 0;
+    while (counter < input.length) {
+      switch(input[counter]) {
+        case ".":
+          jquery_object = jquery_object.getElementsByClassName(input[counter].slice(1));
+          break;
+        case "#":
+          jquery_object = jquery_object.getElementById(input[counter].slice(1));
+          break;
+        default:
+          jquery_object = jquery_object.getElementsByTagName(input[counter]);
+          break;
+        }
+      }
     }
-  };
 
   this.collection = jquery_object;
 
@@ -94,11 +97,50 @@ function jQuery(input) {
 
   this.css = function(style, value) {
     if (value) {
-      return this.collection[0].style[style] = value;
+      for(var i = 0; i < this.collection.length; i++) {
+        this.collection[i].style[style] = value;
+      }
     } else {
       return this.collection[0].style[style];
     }
   }
+
+  this.height = function(newHeight) {
+    if (newHeight) {
+      for(var i = 0; i < this.collection.length; i++) {
+        this.collection[i].style.height = newHeight;
+      }
+    } else {
+      return this.collection[0].style.height;
+    }
+  }
+  this.width = function(newWidth) {
+    if (newWidth) {
+      for(var i = 0; i < this.collection.length; i++) {
+        this.collection[i].style.width = newWidth;
+      }
+    } else {
+      return this.collection[0].style.width;
+    }
+  }
+
+  this.attr = function(attrName, setValue) {
+    if (setValue) {
+      for(var i = 0; i < this.collection.length; i++) {
+        this.collection[i].attributes[attrName] = setValue;
+      }
+    } else
+      return this.collection[0].attributes[attrName];
+  }
+
+  this.html = function(htmlString) {
+    if (htmlString) {
+      return this.collection[0].innerHTML = htmlString;
+    } else {
+      return this.collection[0].innerHTML;
+    }
+  }
+
 }
 
 var $ = jQuery;
