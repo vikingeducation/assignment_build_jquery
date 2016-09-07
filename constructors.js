d542db50272ead7f9533fb5fce5b442939cd10c7
Leo Ahnn <leo.ahnn@gmail.com>
2016-09-07 16:16:28 -0400

    formatting

--- constructors.js
+++ constructors.js
@@ -6,17 +6,13 @@ function FooConstructor() {
 
 function BarConstructor() {
   if (!(this instanceof BarConstructor)) return new BarConstructor();
-
   this.barMethod = function() {};
   this.barProp = "bar!";
 }
 
 function SimpleObject() {
-
-
   this.each = function(collection, func) {
-
-    for(var i=0; i < collection.length; i++) {
+    for (var i = 0; i < collection.length; i++) {
       func(collection[i], i);
     };
   };
