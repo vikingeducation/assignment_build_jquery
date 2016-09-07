102a8aa0190a22b51a7d1fd6c3039671485238cd
David Jiang <davidjgrey@gmail>
2016-09-07 09:44:31 -0700

    working on jQuery

---
+++ building_jQuery.js
@@ -0,0 +1,15 @@
+function jQuery(selector){
+  var results = []
+  if(selector[0] === "."){
+    //look for class
+    results = document.getElementsByClassName(selector.slice(1));
+  }
+  else if (selector[0] === "#"){
+    //look for id
+    results = document.getElementById(selector.slice(1));
+  }
+  else{
+    //look for element
+    results = document.getElementsByTagName(selector);
+  }
+}
