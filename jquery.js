28e8d4d006b35404a80a7240a737d92fb02cc4fc
Leo Ahnn <leo.ahnn@gmail.com>
2016-09-07 13:35:52 -0400

    working on building jquery

--- jquery.js
+++ jquery.js
@@ -1,15 +1,50 @@
 function jQuery(selector) {
-  if (!(this instanceof jQuery)) return new jQuery();
+  if (!(this instanceof jQuery)) return new jQuery(selector);
 
+  this.selector = selector;
+  this.collection = parseSelector(this.selector);
 
-  this.parseSelector = function() {
-    if(selector[0] === "#") {
-
-    } else if(selector[0] === ".") {
-      document.getElementsByClassName()
-
+  function parseSelector(selector) {
+    if (selector[0] === "#") {
+      return document.getElementById(selector.substring(1))
+    } else if (selector[0] === ".") {
+      return document.getElementsByClassName(selector.substring(1))
+    } else if (typeof(selector) === 'object') {
+      return [selector];
     } else {
-
+      return document.getElementsByTagName(selector)
     };
   };
+
+  this.length = this.collection.length;
+
+  this.idx = function(index) {
+    return this.collection[index];
+  }
+
+  this.hasClass = function(klass) {
+    var boolean = false;
+    iterate(function(ele){
+      if (ele.className.split(' ').indexOf(klass) > -1 ) {
+        boolean = true;
+      }
+    }, this.collection)
+    return boolean;
+  }
+
+
+  function iterate(func, collection) {
+    for (var i = 0; i < collection.length; i++) {
+      func(collection[i], i);
+    };
+  };
+
+
 };
+
+
+
+// alias $
+var $ = function(selector) {
+  return jQuery(selector);
+}
