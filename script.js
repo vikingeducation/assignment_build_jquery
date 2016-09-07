8934db59c732d60e34725a0591a319379970f3d4
“Matthew <matthew.hinea@gmail.com>
2016-09-07 10:14:45 -0700

    working on jquery def

--- script.js
+++ script.js
@@ -20,5 +20,41 @@ function SimpleObject() {
     for (var i = 0; i < this.collection.length; i++) {
       method(this.collection[i], i);
     }
+  }; 
+};
+
+SimpleObject.each = function (collection, func) {
+  for (var i = 0; i < collection.length; i++) {
+    func(collection[i], i);
+  }
+};
+
+function jQueryCollection (collection) {
+  this.length = collection.length;
+  this.idx = function (index) {
+    return collection[index];
   };
 };
+
+function jQuery (cssSelector) {
+  console.log(typeof cssSelector);
+
+  if (cssSelector[0] === ".") {
+    var collection = document.getElementsByClassName(cssSelector.substring(1));
+    return new jQueryCollection(collection);
+
+  } else if (cssSelector[0] === "#") {
+    var collection = [document.getElementById(cssSelector.substring(1))]; 
+    return new jQueryCollection(collection);
+
+  } else if (cssSelector[0] === "<") {
+    var collection = [cssSelector];
+    console.log(collection);
+    return new jQueryCollection(collection);
+
+  } else {
+    var collection = document.getElementsByTagName(cssSelector);
+    return new jQueryCollection(collection);
+  };
+}
+
