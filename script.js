1a8f597af763a887db711db49ce3a8f948eb053c
Graham Turner <turnertgraham@gmail.com>
2016-09-07 12:18:11 -0400

    finished warmup

---
+++ script.js
@@ -0,0 +1,65 @@
+function FooConstructor() {
+  this.meth = function() {
+    console.log("Hello world!")
+  };
+
+  this.prop = "Faker";
+};
+
+function BarConstructor() {
+  this.meth = function() {
+    console.log("Bye world!")
+  };
+
+  this.prop = "Baker";
+
+  return this;
+};
+
+function SimpleObject() {
+
+  // var each = function(collect, call) {
+  //   for (var i = 0; i < collect.length; i++) {
+  //     call(collect[i], i);
+  //   }
+  // };
+
+  this.collection = [];
+
+  this.each = function(func) {
+    for (var i = 0; i < this.collection.length; i++) {
+      func(this.collection[i], i);
+    }
+  }
+};
+
+// test = new FooConstructor;
+// test.meth();
+// console.log(test.prop);
+
+// best = new BarConstructor;
+// best.meth();
+// console.log(best.prop);
+// console.log(typeof best);
+// proto = Object.getPrototypeOf(best);
+// console.log(proto);
+
+// myObj = new SimpleObject();
+// myObj.collection = [1,"foo",3];
+// myObj.each( function( el, index ) {
+//   console.log( "Item " + index + " is " + el);
+// });
+
+// SimpleObject.each = function(collect, call) {
+//   for (var i = 0; i < collect.length; i++) {
+//     call(collect[i], i);
+//   };
+// };
+//
+// var collection = ['foo', 'bar', 'fiz', 'baz'];
+//
+//
+//
+// SimpleObject.each(collection, function(el, index) {
+//   console.log( "Item " + index + " is " + el);
+// });
