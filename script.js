9bf27788921bcc78fd3bcf80cc5386357c4bf765
Dylan Lynch <lynchd2@tcnj.edu>
2016-09-07 18:27:44 -0400

    Complete optional

--- script.js
+++ script.js
@@ -33,33 +33,33 @@ function SimpleObject() {
   }
 };
 
-// test = new FooConstructor;
-// test.meth();
-// console.log(test.prop);
+test = new FooConstructor;
+test.meth();
+console.log(test.prop);
 
-// best = new BarConstructor;
-// best.meth();
-// console.log(best.prop);
-// console.log(typeof best);
-// proto = Object.getPrototypeOf(best);
-// console.log(proto);
+best = new BarConstructor;
+best.meth();
+console.log(best.prop);
+console.log(typeof best);
+proto = Object.getPrototypeOf(best);
+console.log(proto);
 
-// myObj = new SimpleObject();
-// myObj.collection = [1,"foo",3];
-// myObj.each( function( el, index ) {
-//   console.log( "Item " + index + " is " + el);
-// });
+myObj = new SimpleObject();
+myObj.collection = [1,"foo",3];
+myObj.each( function( el, index ) {
+  console.log( "Item " + index + " is " + el);
+});
 
-// SimpleObject.each = function(collect, call) {
-//   for (var i = 0; i < collect.length; i++) {
-//     call(collect[i], i);
-//   };
-// };
-//
-// var collection = ['foo', 'bar', 'fiz', 'baz'];
-//
-//
-//
-// SimpleObject.each(collection, function(el, index) {
-//   console.log( "Item " + index + " is " + el);
-// });
+SimpleObject.each = function(collect, call) {
+  for (var i = 0; i < collect.length; i++) {
+    call(collect[i], i);
+  };
+};
+
+var collection = ['foo', 'bar', 'fiz', 'baz'];
+
+
+
+SimpleObject.each(collection, function(el, index) {
+  console.log( "Item " + index + " is " + el);
+});
