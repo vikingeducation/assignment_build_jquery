2a88dcdec484368ce995cfd7412b91bde389e007
Morgan Martin <morganm5201@gmail.com>
2016-09-07 12:49:02 -0400

    simple object

--- script.js
+++ script.js
@@ -6,10 +6,28 @@ function FooConstructor() {
 }
 
 function BarConstructor() {
-  return {
-    barMethod: function() {
-      console.log("Not cool.");
-    },
-    barProp: "foo!"
-  };
+    if (this instanceof BarConstructor) {
+        this.barMethod = function() {
+            console.log("Not cool.");
+        };
+        this.barProp = "foo!";
+    }
+    else {
+        return new BarConstructor();
+    }
+}
+
+function SimpleObject() {
+    this.collection = []
+    this.each = function(func) {
+        for (var i = 0; i < this.collection.length; i++) {
+            func(this.collection[i], i);
+        }
+    }
+}
+
+SimpleObject.each = function(coll, func) {
+    for (var i = 0; i < coll.length; i++) {
+        func(coll[i], i);
+    }
 }
