ea04572ee5aaccbdf1720d767d9c11932dd8c8e2
David J Watts <davidjeremywatts@gmail.com>
2016-09-16 10:48:52 -0700

    finish jquery

--- js.js
+++ js.js
@@ -38,7 +38,7 @@ function jQuery(string) {
     a = document.getElementsByClassName(string.substring(1));
   }
   else if (string[0] === "#") {
-    a = document.getElementById(string.substring(1));
+    a = [document.getElementById(string.substring(1))];
   }
   else {
     a = document.getElementsByTagName(string);
@@ -56,6 +56,78 @@ function jQuery(string) {
       };
     };
     return bool;
+  };
+
+  this.addClass = function(string){
+    for (i = 0; i < a.length; i++) {
+      if (a[i].attributes.class !== undefined) {
+      a[i].attributes.class.value += " " + string.trim();
+    }
+    else {
+      var typ = document.createAttribute("class")
+      typ.value = string.trim();
+      a[i].attributes.setNamedItem(typ);
+    };
+    };
+  };
+
+  this.removeClass = function(string) {
+    var arr = string.trim().split(" ");
+    for (i = 0; i < a.length; i++) {
+      if (a[i].attributes.class !== undefined) {
+        for (k = 0; k < arr.length; k++) {
+          a[i].attributes.class.value = a[i].attributes.class.value.replace(arr[k], "");
+        };
+        a[i].attributes.class.value = a[i].attributes.class.value.trim().replace("  ", "");
+        if (a[i].attributes.class.value === "") {a[i].attributes.removeNamedItem("class")};
+      };
+    };
+  };
+
+  this.toggleClass = function(string) {
+    var arr = string.trim().split(" ");
+    for (i = 0; i < a.length; i++) {
+      if (a[i].attributes.class !== undefined) {
+        for (k = 0; k < arr.length; k++) {
+          if (a[i].attributes.class.value.includes(arr[k])) {
+            a[i].attributes.class.value = a[i].attributes.class.value.replace(arr[k], "");
+          }
+          else {
+            a[i].attributes.class.value += " " + arr[k];
+          };
+        };
+      }
+      else {
+        var typ = document.createAttribute("class");
+        typ.value = string;
+        a[i].attributes.setNamedItem(typ);
+      };
+      a[i].attributes.class.value = a[i].attributes.class.value.trim().replace("  ", " ");
+      if (a[i].attributes.class.value === "") {a[i].attributes.removeNamedItem("class")};
+    };
+  }
+
+  this.css = function(input, istyle ) {
+    if (typeof input === "string" && typeof istyle === "undefined") {
+      return window.getComputedStyle(a[0])[input];
+    } else if (input instanceof Array && typeof istyle === "undefined"){
+      var ans = [];
+      for (i = 0; i < input.length; i++) {
+        ans.push(window.getComputedStyle(a[0])[input[i]]);
+      };
+      return ans;
+    } else if (typeof istyle === "string") {
+        if (a[0].attributes.style !== undefined) {
+          a[i].attributes.class.value += " " + string.trim();
+        } else {
+          var typ = document.createAttribute("class")
+          typ.value = string.trim();
+          a[i].attributes.setNamedItem(typ);
+        };
+    } else if (input instanceof Object) {
+
+    };
+
   };
 
 
