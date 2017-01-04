d28fbee389d9dffd003033619614499dd486bd22
Jessica G <gillan.jessica@gmail.com>
2017-01-04 09:19:36 -0700

    start each function

---
+++ script.js
@@ -0,0 +1,22 @@
+function Foo() {
+  this.someProperty = "Hi";
+  this.someMethod = function() { console.log("this is the function")};
+};
+
+function Bar() {
+  return new Foo()
+}
+
+function Baz() {
+  if (!(this instanceof Baz)) return new Baz();
+  this.someProperty = "Hi";
+  this.someMethod = function() { console.log("this is the function")};
+}
+
+function SimpleObject() {
+  if (!(this instanceof SimpleObject)) return new SimpleObject();
+  this.collection = [1,2,3,4];
+  this.each = function( iterate_func ) {
+    this.collection.forEach( iterate_func );
+  };
+}
