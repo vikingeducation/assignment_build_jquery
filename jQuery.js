f6a407b4c3218e694c5c3bad68a526c92c7258bd
James Harris <computersherpa@gmail.com>
2017-01-04 12:37:24 -0600

    hasClass

--- jQuery.js
+++ jQuery.js
@@ -1,26 +1,37 @@
 function $(input) {
-  this.collection = [1];
+  this.collection = [];
   this.selector = "";
   this.length = undefined;
 
+
+
+  // SETTERS
+
   this.setLength = function() {
     this.length = this.collection.length;
-  }
+  };
 
   this.setProperties = function(query) {
     this.collection = query;
     this.setLength();
-  }
+  };
+
+
+
+  // FINDERS
 
   this.findById = function() {
-    this.setProperties(document.getElementById(this.selector));
+    this.setProperties([document.getElementById(this.selector)]);
   };
   this.findByClass = function() {
-    this.collection = document.getElementsByClassName(this.selector);
+    this.setProperties(document.getElementsByClassName(this.selector));
   };
   this.findByTag = function() {
-    this.collection = document.getElementsByTagName(this.selector);
+    this.setProperties(document.getElementsByTagName(this.selector));
   };
+
+
+  // PARSERS
 
   this.parseSelector = function(input) {
     var inputText = input.slice(1);
@@ -34,24 +45,52 @@ function $(input) {
       this.selector = input;
       this.findByTag();
     }
-  }
+  };
 
   this.parseInput = function(input) {
-    if (input.nodeType === undefined) {
+    if (input instanceof HTMLCollection) {
+      this.setProperties(input);
+    } else if (input.nodeType === undefined) {
       this.parseSelector(input);
     } else if (input.nodeType === 1) {
-      this.collection = [input];
+      this.setProperties([input]);
     }
   };
   this.parseInput(input);
 
+
+  // METHODS
+
   this.idx = function(number) {
     return this.collection[number];
-  }
+  };
+
+  this.each = function(func) {
+    for (var i = 0; i < this.collection.length; i++) {
+      func(this.collection[i], i);
+    }
+  };
+
+  this.hasClass = function(className) {
+    // returns true if at least one element has the class
+    var hasClass = false;
+    this.each(function(element) {
+      if (element.classList.contains(className)) {
+        hasClass = true;
+      }
+    });
+    return hasClass;
+  };
+
+
+
+
+  // RETURN OBJECT
 
   if ( !(this instanceof $) ) {
     return new $(input);
   }
 }
+var jQuery = $;
 
 $('#ul');
