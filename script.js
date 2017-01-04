862f1ed96b22f59c8114e935471ad33b907b69d2
Jessica G <gillan.jessica@gmail.com>
2017-01-04 10:42:03 -0700

    finish basic jQuery functionality

--- script.js
+++ script.js
@@ -1,41 +1,58 @@
-function Foo() {
-  this.someProperty = "Hi";
-  this.someMethod = function() { console.log("this is the function")};
-};
 
-function Bar() {
-  return new Foo()
-}
-
-function Baz() {
-  if (!(this instanceof Baz)) return new Baz();
-  this.someProperty = "Hi";
-  this.someMethod = function() { console.log("this is the function")};
-}
-
-function SimpleObject() {
-  if (!(this instanceof SimpleObject)) return new SimpleObject();
-  this.collection = [1,2,3,4];
-  this.each = function( iterate_func ) {
-    this.collection.forEach( iterate_func );
+  function Foo() {
+    this.someProperty = "Hi";
+    this.someMethod = function() { console.log("this is the function") };
   };
-}
 
-SimpleObject.each = function( collection, iterate_func ) {
-  collection.forEach( iterate_func );
-};
+  function Bar() {
+    return new Foo();
+  };
 
-var jQuery = function(str) {
-  var search = jQuery.searchTypes[str[0]]
-  if ( search ) {
-    return search(str.substr(1))
+  function Baz() {
+    if (!(this instanceof Baz)) return new Baz();
+    this.someProperty = "Hi";
+    this.someMethod = function() { console.log("this is the function")};
+  };
+
+  function SimpleObject() {
+    if (!(this instanceof SimpleObject)) return new SimpleObject();
+    this.collection = [1,2,3,4];
+    this.each = function( iterate_func ) {
+      this.collection.forEach( iterate_func );
+    };
   }
-  else {
-    return document.getElementsByTagName(str)
-  }
-}
 
-jQuery.searchTypes = {
-  '#': function(id) { return document.getElementById(id); },
-  '.': function(klazz) { return document.getElementsByClassName(klazz); }
-};
+  SimpleObject.each = function( collection, iterate_func ) {
+    collection.forEach( iterate_func );
+  };
+
+  var jQuery = function(str) {
+    if (typeof str === 'string')
+      var search = jQuery.searchTypes[str[0]];
+
+      if ( search ) {
+        return JQueryObject( search(str.substr(1)) );
+      }
+      else {
+        return JQueryObject( document.getElementsByTagName(str) );
+      }
+  }
+
+  jQuery.searchTypes = {
+    '#': function(id) { return document.getElementById(id); },
+    '.': function(klazz) { return document.getElementsByClassName(klazz); }
+  }
+
+  // Scrum ?: How is this organized in real jQuery?
+  function JQueryObject(collection) {
+    if (!(this instanceof JQueryObject)) return new JQueryObject(collection);
+
+    this.collection = collection.length ? collection : [collection];
+    this.each = function( iterate_func ) {
+      this.collection.forEach( iterate_func );
+    };
+    this.length = this.collection.length;
+    this.idx = function(index) { return this.collection[index]; };
+    // this.filter = ;
+    // this.find = ;
+  }
