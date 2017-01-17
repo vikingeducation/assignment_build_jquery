6f562a884370c8016d77a824c1cb62b57254e125
Kenny Kottenstette <kennykotten@gmail.com>
2017-01-17 12:08:56 -0800

    utilize querySelectorAll

--- jquery.js
+++ jquery.js
@@ -1,149 +1,137 @@
-function jQuery(selector) {
-  if (this instanceof jQuery) {
-    this.collection = (function() {
-      if (selector[0] === '.') {
-        return document.getElementsByClassName(selector.substring(1));
-      } else if (selector[0] === '#') {
-          return [document.getElementById(selector.substring(1))];
-      } else if (selector instanceof Element) {
-        return [selector];
-      } else {
-        return document.getElementsByTagName(selector);
-      }
-    })();
-    this.length = this.collection.length;
-    this.idx = function(index) {
-      return this.collection[index];
+var jQuery = function(selector) {
+  var domQuery = document.querySelectorAll(selector);
+  return new jQueryObject(domQuery);
+};
+
+function jQueryObject(nodeList) {
+  this.collection = nodeList;
+  this.length = this.collection.length;
+
+  this.idx = function(index) {
+    return this.collection[index];
+  };
+
+  this.each = function(callback){
+    var len = this.length
+
+    for (var i = 0; i < len; i++) {
+      callback(this.collection[i]);
     }
+  };
 
-    this.each = function(callback){
-      var len = this.length
-
-      for (var i = 0; i < len; i++) {
-        callback(this.collection[i]);
+  this.hasClass = function(input) {
+    var match = false;
+    this.each(function(el) {
+      var classes = el.classList;
+      if (classes.contains(input)) {
+        match = true;
       }
-    };
+    })
+    return match;
+  };
 
-    this.hasClass = function(input) {
-      var match = false;
+  this.addClass = function(input) {
+    this.each(function(el) {
+      el.classList.add(input);
+    })
+    return this;
+  };
+
+  this.removeClass = function(input) {
+    this.each(function(el) {
+      el.classList.remove(input);
+    })
+    return this;
+  };
+
+  this.toggleClass = function(input) {
+    this.each(function(el) {
+      el.classList.toggle(input);
+    })
+    return this;
+  };
+
+  this.val = function(input) {
+    if (input) {
       this.each(function(el) {
-        var classes = el.classList;
-        if (classes.contains(input)) {
-          match = true;
-        }
+        el.value = input;
       })
-      return match;
-    };
+    } else {
+      return this.idx(0);
+    }
+  };
 
-    this.addClass = function(input) {
+  this.css = function(cssProperty, cssValue) {
+    if (cssValue) {
       this.each(function(el) {
-        el.classList.add(input);
+        el.style[cssProperty] = cssValue;
       })
-      return this;
-    };
+    } else {
+      return this.idx(0).style[cssProperty] || getComputedStyle(this.idx(0), null)[cssProperty];
+    }
+  };
 
-    this.removeClass = function(input) {
+  this.height = function(pxHeight) {
+    if (pxHeight) {
       this.each(function(el) {
-        el.classList.remove(input);
+        el.style.height = pxHeight + 'px';
       })
-      return this;
-    };
+    } else {
+      return this.idx(0).style.height || getComputedStyle(this.idx(0), null).height;
+    }
+  };
 
-    this.toggleClass = function(input) {
+  this.width = function(pxWidth) {
+    if (pxWidth) {
       this.each(function(el) {
-        el.classList.toggle(input);
+        el.style.width = pxWidth + 'px';
       })
-      return this;
-    };
+    } else {
+      return this.idx(0).style.width || getComputedStyle(this.idx(0), null).width;
+    }
+  };
 
-    this.val = function(input) {
-      if (input) {
-        this.each(function(el) {
-          el.value = input;
-        })
-      } else {
-        return this.idx(0);
-      }
-    };
+  this.attr = function(attributeName, value) {
+    if (value) {
+      this.each(function(el) {
+        el.setAttribute(attributeName, value);
+      })
+    } else {
+      return this.idx(0).getAttribute(attributeName);
+    }
+  };
 
-    this.css = function(cssProperty, cssValue) {
-      if (cssValue) {
-        this.each(function(el) {
-          el.style[cssProperty] = cssValue;
-        })
-      } else {
-        return this.idx(0).style[cssProperty] || getComputedStyle(this.idx(0), null)[cssProperty];
-      }
-    };
-
-    this.height = function(pxHeight) {
-      if (pxHeight) {
-        this.each(function(el) {
-          el.style.height = pxHeight + 'px';
-        })
-      } else {
-        return this.idx(0).style.height || getComputedStyle(this.idx(0), null).height;
-      }
-    };
-
-    this.width = function(pxWidth) {
-      if (pxWidth) {
-        this.each(function(el) {
-          el.style.width = pxWidth + 'px';
-        })
-      } else {
-        return this.idx(0).style.width || getComputedStyle(this.idx(0), null).width;
-      }
-    };
-
-    this.attr = function(attributeName, value) {
-      if (value) {
-        this.each(function(el) {
-          el.setAttribute(attributeName, value);
-        })
-      } else {
-        return this.idx(0).getAttribute(attributeName);
-      }
-    };
-
-    this.html = function(htmlString) {
-      if (htmlString) {
-        this.each(function(el) {
-          el.innerHTML = htmlString;
-        })
-      } else {
-        return this.idx(0).innerHTML;
-      }
-    };
-
-    // return this.collection
-  } else {
-    return new jQuery(selector);
-  }
-}
-
-
+  this.html = function(htmlString) {
+    if (htmlString) {
+      this.each(function(el) {
+        el.innerHTML = htmlString;
+      })
+    } else {
+      return this.idx(0).innerHTML;
+    }
+  };
+};
 
 //alias
 var $ = jQuery;
 
 
-//
-var a = jQuery(".header")
-console.log(a)
 
-var b = jQuery("#title")
-console.log(b)
+// var a = jQuery(".header")
+// console.log(a)
 
-var c = jQuery("p")
-console.log(c)
+// var b = jQuery("#title")
+// console.log(b)
 
-var d = jQuery("asdf")
-console.log(d)
+// var c = jQuery("p")
+// console.log(c)
 
-var some = document.getElementById("example");
-var e = jQuery(some)
-console.log(e)
+// var d = jQuery("asdf")
+// console.log(d)
 
-var all = [a,b,c,d,e]
-all.forEach(function(element){console.log(element.collection)})
+// var some = document.getElementById("example");
+// var e = jQuery(some)
+// console.log(e)
+
+// var all = [a,b,c,d,e]
+// all.forEach(function(element){console.log(element.collection)})
