392f9bf9b58d7750660a938a6c980da5f1d6c81a
Ann Allan <ann.cristina.allan@gmail.com>
2017-02-21 18:50:36 -0500

    Add constructor warmups

--- script.js
+++ script.js
@@ -4,3 +4,16 @@ function Foo() {
         console.log(this.sampleProp);
     };
 }
+
+function Bar() {
+    this.sampleProp = 'Sample Prperty';
+    this.samepleMethod = function() {
+        console.log(this.sampleProp);
+    };
+}
+
+function Baz() {
+    if( !( this instanceof Baz))
+    return new Baz();
+    this.sampleProp = 'Sample Property';
+}
