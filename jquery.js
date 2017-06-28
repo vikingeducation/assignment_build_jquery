4a59efcecf6346f3f3d05c44790e1612c474ae0e
Will Timpson <will.timpson@gmail.com>
2017-06-28 12:20:36 -0400

    basic selectors

--- jquery.js
+++ jquery.js
@@ -1,3 +1,23 @@
 function jQuery(selector) {
-   
+  let selection
+  console.log(selector);
+
+   if (selector[0] === '.') {
+     // select on class
+     selection = document.getElementsByClassName(selector.substring(1));
+     console.log('class');
+
+   } else if (selector[0] === '#') {
+     // select on id
+     selection = document.getElementById(selector.substring(1));
+     console.log('id');
+
+   } else {
+     // select on element
+     selection = document.getElementsByTagName(selector);
+     console.log('element');
+   }
+
+
+   return selection;
 }
