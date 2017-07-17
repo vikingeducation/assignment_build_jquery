6a7ca158178aaec0db05135613c28aff011ec352
anne <richardson.ae@gmail.com>
2017-07-17 09:41:28 -0500

    Create normal constructor

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 $(document).ready(function(){
 
+  // WARMUPS
+
+  // Create a normal constructor called Foo which returns a simple Foo instance containing a sample property and method when you call it with the new keyword.
+
+  function Foo(first,last) {
+    this.firstName = first;
+    this.lastName = last;
+    this.fullName = function(){
+      return this.firstName + " " + this.lastName;
+    };
+  }
+
+  var foo = new Foo("Jane", "Smith");
+  console.log(foo instanceof Object);
+  console.log(foo instanceof Foo);
+
+
+
 
 
 
