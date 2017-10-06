d2220ba78844df5f182a26b1edc5f91dada25bce
Elias Hantula <ehantula@indiana.edu>
2017-10-05 22:06:39 -0400

    third

--- build.js
+++ build.js
@@ -46,30 +46,46 @@ var SimpleObject = function() {
 
 	};
 
-SimpleObject.each= function(collection, funct){
+SimpleObject.each = function (collection, funct) {
 	var i = 0;
 	while (i <collection.length){
 		el = collection[i];
 		index = i;
-		funct(el,index);
+		funct (el,index);
 		i++;
 		};
 	};
 
-	function jQuery(string){
+	function jQuery (string) {
 		     this.collection = [];
 		if (!(this instanceof jQuery)) return new jQuery(string);
-		if (string == null) {this.collection.push(string)}
+		if (typeof string !== 'string') {this.collection.push(string)}
 		else if (string[0] == ".") { this.collection = document.getElementsByClassName(string.slice(1))} else if (
 				string[0] == "#") {this.collection = [document.getElementById(string.slice(1))]} else {
 			this.collection = document.getElementsByTagName(string)};
 			 this.length = this.collection.length;
 			 this.idx = function(num){
 			 return this.collection[num]
-			 }
+			 };
+			 this.hasClass = function (cs) {
+			 var els = document.getElementsByTagName(this);
+             var classes = document.getElementsByClassName(cs);
+             console.log(classes);
+			 };
+			 jQuery.each = function (collect) {
+			 	var i = 0;
+			 	var result = [];
+			 	while (i < collect.length){
+			 	var el = collect[i];
+			 	var idx = i;
+			 	i++
+			 	return el;
+			 	};
+
+			 };
 
 			};
-				
+		var $ = jQuery;	
 		 
 	
 
