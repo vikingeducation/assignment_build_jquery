5037c9af90f35b060721b4719f6a183793ff9ee3
Davey Wright <davey.w.wright@gmail.com>
2017-11-06 12:24:17 +0000

    refactor SimpleObject each method to take multiple parameters

--- index.js
+++ index.js
@@ -69,26 +69,40 @@ console.log(baz instanceof Baz);
 
 
 
-// Constructor that can take a callback as a function
-function SimpleObject(collection){
-	this.collection = collection;
-	this.each = (callback) => {
+
+// Constructor with method that can take callback and other arguments
+function SimpleObject(){
+	this.collection = [];
+	this.each = (...args) => {
+		// let callback equal last argument if it is a function
+		let callback = (args[args.length - 1] instanceof Function) ? args[args.length - 1] : null;
+		// this.collection = first argument if it is an array
+		this.collection = (args[0] instanceof Array) ? args[0]: this.collection;
+		// loop through each item in collection and invoke callback
 		this.collection.forEach(callback);
 	}
 }
 
 myObj = new SimpleObject();
+
+// Callback only
 myObj.collection = [1,"foo",3];
 myObj.each( function( el, index ) {
   console.log( "Item " + index + " is " + el);
-})
-
+});
 // Item 0 is 1
 // Item 1 is foo
 // Item 2 is 3
 
-
-
+// Callback and collection
+let collection = ['foo', 'bar', 'fiz', 'baz'];
+myObj.each(collection, function(el, index) {
+  console.log( "Item " + index + " is " + el);
+});
+// Item 0 is foo
+// Item 1 is bar
+// Item 2 is fiz
+// Item 3 is baz
 
 
 
