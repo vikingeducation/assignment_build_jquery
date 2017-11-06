1f7f6223e3e98bfd5b2860a272b637ca79c0b579
Davey Wright <davey.w.wright@gmail.com>
2017-11-06 10:35:31 +0000

    create SimpleObject constructor

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Class
 class FooBar {
 	constructor(){
 		this.name = 'FooBar class';
@@ -16,6 +17,7 @@ foobar.method();
 
 
 
+// Constructor
 function Foo(){
 	this.name = 'Foo Constructor';
 	this.method = () => console.log(`This is the ${this.name}`);;
@@ -29,6 +31,8 @@ console.log(foo instanceof Foo);
 foo.method();
 
 
+
+// Function that returns simple object
 let Bar = function(){
 	let shaka = {
 		name: 'Boom Shake Shake The Room',
@@ -47,10 +51,41 @@ bar.method();
 
 
 
+// Constructor Baz can be invoked without new keyword
+function Baz(){
+	this.name = 'Baz Constructor';
+	if(!(this instanceof Baz)){
+		return new Baz();
+	}
+}
+
+var baz = new Baz();
+console.log(baz instanceof Baz);
+//=> true
+
+baz = Baz();
+console.log(baz instanceof Baz);
+//=> true
 
 
 
+// Constructor that can take a callback as a function
+function SimpleObject(collection){
+	this.collection = collection;
+	this.each = (callback) => {
+		this.collection.forEach(callback);
+	}
+}
 
+myObj = new SimpleObject();
+myObj.collection = [1,"foo",3];
+myObj.each( function( el, index ) {
+  console.log( "Item " + index + " is " + el);
+})
+
+// Item 0 is 1
+// Item 1 is foo
+// Item 2 is 3
 
 
 
