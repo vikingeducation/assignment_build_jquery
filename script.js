3f898488602e736616f834ad36bc5affe59ad985
Holly Erickson <Erickson.Holly@icloud.com>
2017-11-16 14:42:00 -0600

    Add Baz constructor

--- script.js
+++ script.js
@@ -23,10 +23,33 @@ let Bar = function() {
 }
 
 let bar = Bar();
-//essentially bar is the return value of Bar(), which is barObj
+//essentially bar is the return value of Bar, which is barObj
 console.log(bar.otherMethod());
 //My name is Bar!
 console.log(bar instanceof Object);
 //true
 console.log(bar instanceof Bar);
 //false
+
+//constructor Baz, can return instance without using 'new'
+function Baz() {
+  if (!(this instanceof Baz)) {
+    return new Baz();
+  }
+  this.name = 'Baz!',
+  this.someMethod = () => `My name is ${this.name}`
+}
+
+//invoke Baz without new
+let baz = Baz();
+console.log(baz.someMethod());
+//My name is Baz!
+console.log(baz instanceof Baz);
+//true
+
+//can still invoke Baz with new
+baz = new Baz();
+console.log(baz.someMethod());
+//My name is Baz!
+console.log(baz instanceof Baz);
+//true
