b37044f0110c14d0dffca23cea33f5f62607332b
Brennan_Fulmer <31666354+BrennanFulmer@users.noreply.github.com>
2018-01-15 01:59:29 -0500

    finished second part, i.e. class or id only searching works now

--- build_jq.js
+++ build_jq.js
@@ -3,11 +3,11 @@
  cd Documents/Viking/JS/build_jq
 */
 
-/*Build the jQuery() function, which takes a string representing a CSS
+/*1. Build the jQuery() function, which takes a string representing a CSS
 selector and returns a collection of elements (or the single element) that
 match it.
 
-Add the following functionality: Note: assume only single selectors and not
+2. Add the following functionality: Note: assume only single selectors and not
 chained or nested ones like ".some-class someElement"
   Classes, e.g. jQuery(".some-class")
   IDs, e.g. jQuery("#some-id")
@@ -18,34 +18,33 @@ window.top.onload = function() {
 
   function jQuery(selector) {
     var elements = [];
-    var nodes = null
+    var nodes = null;
+    var term = selector.substr(1);
 
-    if (selector[0] = ".") {
-      nodes = document.getElementsByClassName(selector.substr(1));
-    } else if (selector[0] = "#") {
-      nodes = document.getElementById(selector.substr(1));
-    } else if (selector[0] = "<") {
-        if (selector[1] = "/") {
-          nodes = document.getElementsByTagName(selector.slice(2, -1));
-        } else {
-          nodes = document.getElementsByTagName(selector.slice(1, -1));
-        }
+    if (selector[0] === ".") {
+      nodes = document.getElementsByClassName(term);
+    } else if (selector[0] === "#") {
+      nodes = document.getElementById(term);
     } else {
       nodes = document.querySelectorAll(selector);
     }
 
-/*
-document.getElementsByClassName(".some-class");
-document.getElementById("#some-id");
-document.getElementsByTagName("<some-tag> or </some-tag>");
-document.querySelectorAll("tag.class");
-*/
-
-    var loopy = 0;
-    while (loopy < nodes.length) {
-      elements.push(nodes[loopy]);
-      loopy++;
+    if (nodes[0] === undefined && nodes.length === 0) {
+      elements = null;
+    } else if (nodes === null) {
+      elements = null;
+    } else if (nodes[0] === undefined) {
+      elements.push(nodes);
+    } else if (nodes.length < 2) {
+      elements.push(nodes[0]);
+    } else {
+      var indexy = 0;
+      while (indexy < nodes.length) {
+        elements.push(nodes[indexy]);
+        indexy++;
+      }
     }
+
     return elements;
   }
 };
