3f9e79af85b52ad7eeb0c7d97cdf7fcb6df239b4
Brennan_Fulmer <31666354+BrennanFulmer@users.noreply.github.com>
2018-01-16 02:43:05 -0500

    revision successful, switched logic from if statements to case statements

--- build_jq.js
+++ build_jq.js
@@ -11,36 +11,43 @@ window.onload = function() {
   function jQuery(selector) {
     var elements = [];
 
-
-    if (selector[0] === ".") {
-      var nodes = document.getElementsByClassName(selector.substr(1));
-    } else if (selector[0] === "#") {
-      var nodes = document.getElementById(selector.substr(1));
-    } else {
-      // for html tags, also works for tag.class
-      var nodes = document.querySelectorAll(selector);
+    switch (selector[0]) {
+      case ".":
+        var nodes = document.getElementsByClassName(selector.substr(1));
+      break;
+      case "#":
+        var nodes = document.getElementById(selector.substr(1));
+      break;
+      default:
+        var nodes = document.querySelectorAll(selector);
+        // for html tags, also works for tag.class
     }
 
-
-    if (nodes === null) {
-      // no results
-    } else if (nodes[0] === undefined && nodes.length === 0) {
-      // empty nodelists
-    } else if (nodes[0] === undefined) {
-      // for ID's
-      elements.push(nodes);
-    } else if (nodes.length === 1) {
-      // single class or html tag
-      elements.push(nodes[0]);
-    } else {
-      var indexy = 0;
-      while (indexy < nodes.length) {
-        elements.push(nodes[indexy]);
-        indexy++;
-      }
+    switch (true) {
+      case nodes === null:
+        // no results
+      break;
+      case nodes[0] === undefined && nodes.length === 0:
+        // empty nodelists
+      break;
+      case nodes[0] === undefined:
+        elements.push(nodes);
+        // for ID's
+      break;
+      case nodes.length === 1:
+        elements.push(nodes[0]);
+        // nodelists with 1 item
+      break;
+      default:
+        var indexy = 0;
+        while (indexy < nodes.length) {
+          elements.push(nodes[indexy]);
+          indexy++;
+        }
     }
 
-    
     return elements;
   }
+
+
 };
