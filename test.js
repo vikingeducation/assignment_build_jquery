b10ca539cb1ef1914c464555729427c36916e421
Brennan_Fulmer <31666354+BrennanFulmer@users.noreply.github.com>
2018-01-16 18:26:58 -0500

    trying to rework so that I can add the other jQuery functionality

---
+++ test.js
@@ -0,0 +1,85 @@
+
+window.onload = function() {
+  jQuery = $ = jQueryWrapper;
+
+  function jQueryWrapper(selector) {
+    return new jQueryObject(selector);
+  }
+
+  function jQueryObject(selector) {
+    if (typeof selector === "string") {
+      switch (selector[0]) {
+        case ".":
+          var nodes = document.getElementsByClassName(selector.substr(1));
+        break;
+        case "#":
+          var nodes = document.getElementById(selector.substr(1));
+        break;
+        default:
+          var nodes = document.querySelectorAll(selector);
+          // for html tags, also works for tag.class
+      }
+
+        switch (true) {
+          case nodes === null:
+            // no results
+          break;
+          case nodes[0] === undefined && nodes.length === 0:
+            // empty nodelists
+          break;
+          case nodes[0] === undefined:
+            collection.push(nodes);
+            // for ID's
+          break;
+          case nodes.length === 1:
+            collection.push(nodes[0]);
+            // nodelists with 1 item
+          break;
+          default:
+            var indexy = 0;
+            while (indexy < nodes.length) {
+              collection.push(nodes[indexy]);
+              indexy++;
+            }
+        }
+        return collection;
+    } else {
+      return [selector];
+    }
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+};
