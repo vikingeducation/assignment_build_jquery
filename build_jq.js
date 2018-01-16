04cfa8ce2a21940d37a1f8d3cbc7a9dda5928cfb
Brennan_Fulmer <31666354+BrennanFulmer@users.noreply.github.com>
2018-01-15 23:06:25 -0500

    had to revert some changes, saving before trying to re-implement them

--- build_jq.js
+++ build_jq.js
@@ -1,39 +1,33 @@
-/* 1. Build the jQuery() function, which takes a string representing a CSS
-selector and returns a collection of elements (or the single element) that
-match it. Make this function available when your page loads so you can play
-with it in the console.
 
-2. Add the following functionality: Note: assume only single selectors and not
-chained or nested ones like ".some-class someElement"
-  Classes, e.g. jQuery(".some-class")
-  IDs, e.g. jQuery("#some-id")
-  Elements, e.g. jQuery("div") */
+/*
+ cd Documents/Viking/JS/build_jq
 
-window.top.onload = function() {
+ test = $("#fish")
+*/
+
+window.onload = function() {
   $ = jQuery;
 
   function jQuery(selector) {
     var elements = [];
-    var nodes = null;
-    var term = selector.substr(1);
 
     if (selector[0] === ".") {
-      nodes = document.getElementsByClassName(term);
+      var nodes = document.getElementsByClassName(selector.substr(1));
     } else if (selector[0] === "#") {
-      nodes = document.getElementById(term);
+      var nodes = document.getElementById(selector.substr(1));
     } else {
       // for html tags, also works for tag.class
-      nodes = document.querySelectorAll(selector);
+      var nodes = document.querySelectorAll(selector);
     }
 
-    if (nodes[0] === undefined && nodes.length === 0) {
-      // empty nodelists
-    } else if (nodes === null) {
+    if (nodes === null) {
       // no results
+    } else if (nodes[0] === undefined && nodes.length === 0) {
+      // empty nodelists
     } else if (nodes[0] === undefined) {
       // for ID's
       elements.push(nodes);
-    } else if ((nodes.length = 1)) {
+    } else if (nodes.length === 1) {
       // nodelists with 1 item
       elements.push(nodes[0]);
     } else {
@@ -46,26 +40,3 @@ window.top.onload = function() {
     return elements;
   }
 };
-
-/*
- cd Documents/Viking/JS/build_jq
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// spacing
