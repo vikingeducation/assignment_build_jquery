69ce3f0f604ebdfd94759b802643cb5d3a9ef04a
Brennan_Fulmer <31666354+BrennanFulmer@users.noreply.github.com>
2018-01-17 00:18:31 -0500

    realized what might be the 4th different approach now wont work, saving prior to another rework - just to be clear the reworks are in the test files to make everything easier for me to follow, will consolidate after a successfully option is found

--- test.js
+++ test.js
@@ -2,12 +2,19 @@
 window.onload = function() {
   jQuery = $ = jQueryWrapper;
 
+/* the key is this.whatever - need to base this approach off of saving the
+whole function into a variable, and by default during that initial action
+returning the search result */
+
   function jQueryWrapper(selector) {
     return new jQueryObject(selector);
   }
 
   function jQueryObject(selector) {
-    if (typeof selector === "string") {
+    var collection = [];
+    if (typeof selector !== "string") {
+      return [selector];
+    } else {
       switch (selector[0]) {
         case ".":
           var nodes = document.getElementsByClassName(selector.substr(1));
@@ -20,32 +27,27 @@ window.onload = function() {
           // for html tags, also works for tag.class
       }
 
-        switch (true) {
-          case nodes === null:
-            // no results
-          break;
-          case nodes[0] === undefined && nodes.length === 0:
-            // empty nodelists
-          break;
-          case nodes[0] === undefined:
-            collection.push(nodes);
-            // for ID's
-          break;
-          case nodes.length === 1:
-            collection.push(nodes[0]);
-            // nodelists with 1 item
-          break;
-          default:
-            var indexy = 0;
-            while (indexy < nodes.length) {
-              collection.push(nodes[indexy]);
-              indexy++;
-            }
-        }
-        return collection;
-    } else {
-      return [selector];
+      switch (true) {
+        case nodes === null:
+          // no results
+        break;
+        case nodes[0] === undefined && nodes.length === 0:
+          // empty nodelists
+        break;
+        case nodes.length === 1:
+          collection.push(nodes[0]);
+          // nodelists with 1 item
+        break;
+        default:
+          collection.push(nodes);
+      }
+      return collection[0];
     }
+
+    console.log("fish");
+
+    this.length = collection.length;
+
   }
 
 
