cc1a1ed345e25e2a3e44b1d0a661dace8bb371b2
Brennan_Fulmer <31666354+BrennanFulmer@users.noreply.github.com>
2018-01-18 18:49:20 -0500

    finished each rework and hasClass

--- build_jq.js
+++ build_jq.js
@@ -1,7 +1,7 @@
 /*
  cd Documents/Viking/JS/build_jq
 
- var test = $("h4")
+ var test = $("h1")
 */
 
 window.onload = function() {
@@ -28,24 +28,25 @@ window.onload = function() {
       return "Only strings or single DOM objects are accepted";
     }
 
-      switch (true) {
-        case nodes === null:
-          nodes = [];
-          // no ID results
-          break;
-        case Array.isArray(nodes):
-          // do nothing when passing DOM object
-          break;
-        case nodes.length < 2 || nodes[0] === undefined:
-          collection.push(nodes);
-          break;
-        default:
-          var indexy = 0;
-          while (indexy < nodes.length) {
-            collection.push(nodes[indexy]);
-            indexy++;
-          }
-      }
+    switch (true) {
+      case nodes === null:
+        nodes = [];
+        // no ID results
+        break;
+      case nodes[0] === undefined:
+        collection.push(nodes);
+        // for IDs
+        break;
+      case Array.isArray(nodes):
+        // do nothing when passing DOM object
+        break;
+      default:
+        var indexy = 0;
+        while (indexy < nodes.length) {
+          collection.push(nodes[indexy]);
+          indexy++;
+        }
+    }
 
     var wrapper = nodes;
 
@@ -56,24 +57,25 @@ window.onload = function() {
     };
 
     wrapper.each = function(yourFunction) {
-      for (var index = 0; index < collection.length; index++) {
-        yourFunction(collection[index], index);
+      var indexy = 0;
+      while (indexy < collection.length) {
+        yourFunction(collection[indexy], indexy);
+        indexy++;
       }
     };
 
     // className must be in string form
     wrapper.hasClass = function(className) {
-
+      var result = false;
       wrapper.each(function(item) {
-        if (item.classList.contains(className)) {
-          return true;
-        } else {
-          return false;
+        if (item.classList === undefined) {
+          // if an item has no classes always stay false
+        } else if (item.classList.contains(className)) {
+          result = true;
         }
-      }
-    )
+      });
       return result;
-    }
+    };
 
     return wrapper;
   }
